1368c86c2824372492cd61eafabf56e546d7d415
edisonmora95 <edison_andre_9@hotmail.com>
2019-07-28 20:39:33 -0500

    Sidenav && routing

---
+++ src/components/Sidenav.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-list>
+    <q-item clickable :to="localizacion">
+      <q-item-section>
+        <q-item-label>Ingresar calificacion</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item >
+      <q-item-section>
+        <q-item-label>Formularios guardados</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item >
+      <q-item-section>
+        <q-item-label>Cerrar sesion</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        localizacion: { name: "localizacion" },
+        formularios: { name: "formularios" }
+      };
+    }
+  };
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="drawer = !drawer"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          {{ routeName }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-drawer v-model="drawer" bordered content-class="bg-grey-2">
+      <sidenav></sidenav>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from "quasar";
+import Sidenav from "../components/Sidenav.vue";
+
+export default {
+  name: "MyLayout",
+  data() {
+    return {
+      drawer: false
+    };
+  },
+  computed: {
+    routeName() {
+      return this.$route.meta.title;
+    }
+  },
+  methods: {
+    openURL
+  },
+  components: {
+    Sidenav
+  }
+};
+</script>
+
+<style></style>
