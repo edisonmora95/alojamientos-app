88450956a1067979336a0fc8c358b8bf6b751776
edisonmora95 <edison_andre_9@hotmail.com>
2019-08-03 22:30:13 -0500

    Componente Amenaza

---
+++ src/components/Form/Amenaza.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <main class="row">
+      <section class="col-xs-12 col-sm-6 q-my-sm q-px-sm">
+        <q-select
+          label="Tipo de amenaza"
+          v-model="tipoAmenaza"
+          :options="tiposAmenaza"
+          :disable="!isNewAmenaza"
+          outlined
+        ></q-select>
+      </section>
+      <section class="col-xs-12 col-sm-6 q-my-sm q-px-sm">
+        <q-select
+          label="Nivel de recurrencia"
+          v-model="nivelRecurrencia"
+          :options="nivelesRecurrencia"
+          :disable="!isNewAmenaza"
+          outlined
+          emit-value
+          map-options
+        ></q-select>
+      </section>
+    </main>
+    <footer class="row">
+      <section class="col-xs-4 offset-xs-4 text-center" v-if="isNewAmenaza">
+        <q-btn
+          color="primary"
+          :disable="btnDisabled"
+          @click="addAmenaza"
+        >Aceptar</q-btn>
+      </section>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isNewAmenaza: {
+      type: Boolean,
+      default: false,
+      description: "Indica si el componente se esta usando para ingresar una nueva amenaza o para mostrar una amenaza ya ingresada"
+    },
+    amenaza: {
+      required: false,
+      default: function() {
+        return {
+          tipoAmenaza: "",
+          nivelRecurrencia: ""
+        };
+      }
+    }
+  },
+  mounted() {
+    this.copyPropValues();
+  },
+  data() {
+    return {
+      tipoAmenaza: "",
+      nivelRecurrencia: "",
+
+      tiposAmenaza: [
+        "Ninguno",
+        "Movimiento en masa",
+        "Inundacion",
+        "Incendio Forestal",
+        "Erupcion Volcanica",
+        "Tsunami",
+        "Cambio Climatico",
+        "Deficit hidrico",
+        "Sequia",
+        "Oleajes"
+      ],
+      nivelesRecurrencia: [
+        "Alto",
+        "Medio",
+        "Bajo"
+      ]
+    };
+  },
+  computed: {
+    btnDisabled() {
+      return (
+        this.tipoAmenaza == "" ||
+        this.nivelRecurrencia == ""
+      );
+    }
+  },
+  methods: {
+    addAmenaza() {
+      const payload = {
+        tipoAmenaza: this.tipoAmenaza,
+        nivelRecurrencia: this.nivelRecurrencia
+      };
+      this.$emit("addAmenaza", payload);
+      this.clearEvent();
+    },
+    clearEvent() {
+      this.tipoAmenaza = "";
+      this.nivelRecurrencia = ""
+    },
+    copyPropValues() {
+      this.tipoAmenaza = this.amenaza.tipoAmenaza;
+      this.nivelRecurrencia = this.amenaza.nivelRecurrencia;
+    }
+  }
+};
+</script>
